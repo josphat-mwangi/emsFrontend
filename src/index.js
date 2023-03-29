762f3476aaa4b4ee9070691cf72ad67dea34d714
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-03-29 16:29:23 +0300

    doo perstist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -9,8 +9,9 @@ import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
 
 let persistor = persistStore(store);
+console.log("store", store)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
---
+++ src/utils/axios.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_REST_API_URL,
+});
+
+console.log("api working",  process.env.REACT_APP_SERVER_REST_API_URL)
