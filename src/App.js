499c3e0fd6188140116f731f83d80c3666c83ed3
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-03-29 16:27:19 +0300

    do routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import Empoyee from './pages/Empoyee';
+import { fetchAllEmployees  } from './features/employeeReducer';
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchAllEmployees())
+  }, [dispatch])
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route element={<Empoyee />} path="/" />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
